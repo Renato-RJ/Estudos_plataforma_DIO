e8d9403a361d449769b3bb54c59fe979ca851dd9
Renato-RJ <renato_santosgs@hotmail.com>
2022-06-17 19:29:45 -0300

    Aperfeiçoando Anagramas, dupla checagem.

--- Java/Metodos/src/main/java/metodos/Anagramas.java
+++ Java/Metodos/src/main/java/metodos/Anagramas.java
@@ -2,15 +2,16 @@ package metodos;
 
 public class Anagramas {
 	public static void main(String[] args) {
-	System.out.println("As palavras são anagramas? " + anagramas("zzzzr", "cooar"));
+		System.out.println("As palavras são anagramas? " + anagramas("corra", "cooar"));
 	}
+
 	public static Boolean anagramas(String a, String b) {
-		Boolean eAnagrama=false;
-		if (a.length()==b.length()) {
-			for (int i=0; i<a.length(); i++) {
-				eAnagrama=b.contains(a.substring(i, i+1));
-				if (eAnagrama==false) {
-				break;
+		Boolean eAnagrama = false;
+		if (a.length() == b.length()) {
+			for (int i = 0; i < a.length(); i++) {
+				eAnagrama = b.contains(a.substring(i, i + 1)) && a.contains(b.substring(i, i + 1));
+				if (eAnagrama == false) {
+					break;
 				}
 			}
 		}
